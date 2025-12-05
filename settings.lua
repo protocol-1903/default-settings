4294fda82979e14fabda357f4974f7a060714cd8
protocol-1903 <fisher.braydon@yahoo.com>
2025-12-04 21:44:18 -0700

    Added required references for PEREL

---
+++ settings.lua
@@ -0,0 +1,3 @@
+data.raw["bool-setting"].perel_circuit_wire_removed.forced_value = true
+data.raw["bool-setting"].perel_pre_circuit_wire_added.forced_value = true
+data.raw["bool-setting"].perel_circuit_network_destroyed.forced_value = true
